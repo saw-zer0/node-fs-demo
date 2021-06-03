9a789e9baf9aa7950b1165e874a06fc67a2d4d4b
Sanil Shakya <sanilshakya9gmail.com>
2021-06-03 10:21:41 +0545

    methods demo added

--- filesystem.js
+++ filesystem.js
@@ -3,32 +3,31 @@ const path = require('path');
 
 
 function writeFile(pathToFile,fileName, text){
-    console.log('helloworkd')
     fs.writeFile(path.join(pathToFile,fileName), text,(err)=>{
         if(err) throw err;
         console.log('file written');
     })    
 }
 
-function appendToFile(path, fileName, text){
+function appendToFile(pathToFile, fileName, text){
     fs.appendFile(path.join(pathToFile, fileName), text,(err)=>{
         if(err) throw err;
         console.log('file appended')
     })
 }
 
-function reaadFile(pathToFile, fileName, text){
-    fs.readFile(path.join(path, fileName),{encoding: utf-8},(err, data)=>{
+function readFile(pathToFile, fileName){
+    fs.readFile(path.join(pathToFile, fileName),{encoding: 'utf-8'},(err, data)=>{
         if(err) throw err;
         console.log(data);
     })
 }
 
 function deleteFile(pathToFile, fileName){
-    fs.unlink(path.join(path, fileName), (err) => {
+    fs.unlink(path.join(pathToFile, fileName), (err) => {
         if(err) throw err;
         console.log('file deleted');
     })
 }
 
-module.exports = {writeFile, appendToFile, reaadFile, deleteFile};
+module.exports = {writeFile, appendToFile, readFile, deleteFile};
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const fs = require('./filesystem');
+const path = require('path')
 
 fs.writeFile('./','text.txt', 'Hello World');
+fs.appendToFile(__dirname, 'text.txt', '!!! \n hello again');
+fs.readFile(__dirname, 'text.txt');
+fs.writeFile(__dirname, 'fileToDelete', 'hello once more');
+fs.deleteFile(__dirname, 'fileToDelete')
